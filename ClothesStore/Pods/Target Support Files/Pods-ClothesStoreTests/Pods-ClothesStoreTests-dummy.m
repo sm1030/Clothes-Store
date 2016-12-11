d49b0037d90f9a0861e0936067fb8ed3c9695326
Alexandre Malkov <sm1030@gmail.com>
2016-12-11 16:33:48 +0000

    Project converted into Workspace with Pods

---
+++ ClothesStore/Pods/Target Support Files/Pods-ClothesStoreTests/Pods-ClothesStoreTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ClothesStoreTests : NSObject
+@end
+@implementation PodsDummy_Pods_ClothesStoreTests
+@end
---
+++ ClothesStore/Pods/Target Support Files/Pods-ClothesStoreUITests/Pods-ClothesStoreUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ClothesStoreUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ClothesStoreUITests
+@end
